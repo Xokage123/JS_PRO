61e3fa61bc34e3e99df3c961a264692394f49e37
kaido <maxartem0419@gmail.com>
2021-09-09 23:45:43 +0300

    update

---
+++ module_8/src/api/card-info.js
@@ -0,0 +1,7 @@
+import CardInfo from 'card-info';
+
+export const generateInfoCard = (number) => {
+  const cardInfo = new CardInfo(number);
+  // console.log('Название банка:', cardInfo.bankName);
+  // console.log('Логотип банка:', cardInfo.bankLogo);
+}
