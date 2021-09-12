898a2b8013d88cbc6957be127a13eb5b9370668f
kaido <maxartem0419@gmail.com>
2021-09-12 18:19:04 +0300

    update project

--- module_8/src/index.js
+++ module_8/src/index.js
@@ -1,8 +1,11 @@
 import {
     mount
 } from "redom";
-// import JustValidate from "just-validate"
 import Inputmask from "inputmask"
+import CardInfo from "card-info"
+import {
+    Moon
+} from "./utils/index"
 import {
     nameInputs
 } from "./data/index.js"
@@ -13,10 +16,10 @@ import DOM, {
 const maskNumberInput = new Inputmask("9999 9999 9999 9999");
 maskNumberInput.mask(DOM.form.inputs.number.element);
 
-// console.log(JustValidate);
+const maskDateInput = new Inputmask("99/99");
+maskDateInput.mask(DOM.form.inputs.date.element);
 
 const checkInput = (ev, value) => {
-    console.log(value);
     const input = ev.target;
     const valueInput = input.inputmask ? input.inputmask.unmaskedvalue() : input.value;
     const nameInput = input.name;
@@ -46,13 +49,21 @@ const checkInput = (ev, value) => {
     return checkValue;
 }
 
-const validateAllInputs = inputs => {
-    let check = true;
-    for (let name in inputs) {
-        // !inputs[name] ? check = false : null;
+DOM.form.inputs.number.element.addEventListener('input', ev => {
+    const value = ev.target.inputmask.unmaskedvalue();
+    const cardInfo = new CardInfo(value);
+    if (cardInfo.bankAlias) {
+        DOM.bank.nameBank.element.innerHTML = `Название банка: ${bankAlias}`;
+    } else {
+        DOM.bank.nameBank.element.innerHTML = ""
     }
-    return check;
-}
+    if (cardInfo.brandAlias) {
+        DOM.bank.nameBrand.element.innerHTML = `Платежная система: ${cardInfo.brandName}`;
+    } else {
+        DOM.bank.nameBrand.element.innerHTML = "";
+    }
+    console.log(cardInfo);
+});
 
 for (let nameInput in DOM.form.inputs) {
     DOM.form.inputs[nameInput].element.addEventListener('input', ev => {
@@ -68,20 +79,19 @@ DOM.form.element.append(
     DOM.form.inputs.mail.element,
     DOM.form.buttons.submit.element)
 DOM.container.element.append(
-    DOM.bank.logoContainer.element,
+    DOM.bank.nameBrand.element,
     DOM.bank.nameBank.element,
     DOM.form.element);
 
 mount(document.body, DOM.container.element);
 
-console.log(DOM.form.class[0]);
 new JustValidate(`.${DOM.form.class[0]}`, {
     rules: {
         [nameInputs.number]: {
             required: true,
             function: (name) => {
                 const realyValue = DOM.form.inputs[name].element.inputmask ? DOM.form.inputs[name].element.inputmask.unmaskedvalue() : DOM.form.inputs[name].element.value;
-                return realyValue.length === 16;
+                return (realyValue.length === 16 && Moon(realyValue));
             }
         },
         [nameInputs.mail]: {
@@ -96,12 +106,18 @@ new JustValidate(`.${DOM.form.class[0]}`, {
         },
         [nameInputs.date]: {
             required: true,
+            function: (name, value = "") => {
+                const splitdate = value.split('/');
+                const mounthEnd = Number(splitdate[0]);
+                const yearEnd = Number(splitdate[1]);
+                return (mounthEnd > 12 || mounthEnd === 0 || yearEnd < new Date().getFullYear().toString().slice(2));
+            }
         }
     },
     messages: {
         [nameInputs.number]: {
             required: 'Данное поле обязательно для заполнения',
-            function: 'Вы ввели меньше 16 обязательных символов'
+            function: 'Проверка номера не прошла, попробуйте еще раз!'
         },
         [nameInputs.mail]: {
             required: 'Данное поле обязательно для заполнения',
@@ -113,6 +129,7 @@ new JustValidate(`.${DOM.form.class[0]}`, {
         },
         [nameInputs.date]: {
             required: 'Данное поле обязательно для заполнения',
+            function: 'Вы ввели неверную дату'
         }
     },
 });
